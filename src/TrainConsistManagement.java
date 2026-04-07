87029b240d3fe0c28632cf202449ed9dcbf1e949
akxx45 <as8373@srmist.edu.in>
2026-04-07 09:11:28 +0530

    Base application setup

---
+++ src/TrainConsistManagement.java
@@ -0,0 +1,51 @@
+public class TrainConsistManagement {
+
+    static class CargoSafetyException extends RuntimeException {
+        public CargoSafetyException(String message) {
+            super(message);
+        }
+    }
+
+    static class GoodsBogie {
+        String shape;
+        String cargo;
+
+        GoodsBogie(String shape) {
+            this.shape = shape;
+        }
+
+        void assignCargo(String cargo) {
+            try {
+                if (shape.equalsIgnoreCase("Rectangular") &&
+                        cargo.equalsIgnoreCase("Petroleum")) {
+
+                    throw new CargoSafetyException("Error: Unsafe cargo assignment!");
+                }
+
+                this.cargo = cargo;
+                System.out.println("Cargo assigned successfully -> " + cargo);
+
+            } catch (CargoSafetyException e) {
+                System.out.println(e.getMessage());
+
+            } finally {
+                System.out.println("Cargo validation completed for " + shape + " bogie\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("UC15 - Safe Cargo Assignment");
+        System.out.println("===================================");
+
+        GoodsBogie cylindrical = new GoodsBogie("Cylindrical");
+        cylindrical.assignCargo("Petroleum");
+
+        GoodsBogie rectangular = new GoodsBogie("Rectangular");
+        rectangular.assignCargo("Petroleum");
+
+        System.out.println("UC15 runtime handling completed...");
+    }
+}
